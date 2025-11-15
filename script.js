0902b9abc924e8d8620b2ff10b1dd2b83b7030ed
Swarup Das <swarupdas2046@gmail.com>
2025-11-15 22:58:37 +0530

    Practice

---
+++ script.js
@@ -0,0 +1,125 @@
+
+let arr = [
+
+    {
+        "team": "CSK",
+        "primary-color": "yellow",
+        "secondary-color": "blue",
+        "captain": "Ruturaj Gaikwad",
+        "image": "https://media.tenor.com/orxm7U5q4w8AAAAM/csk-whistle-podu-army.gif",
+        "title": "5 titles (2010 , 2011 , 2018, 2021 , 2023)",
+    },
+    {
+        "team": "Mi",
+        "primary-color": "blue",
+        "secondary-color": "gold",
+        "captain": "Hardik Pandya",
+        "image": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhBKFfR7h8S9hnGVJ7a8KRXSdyw5X_W5o_nRyRrl8GZlVJeG3nfhYvukK03l3bvjb0mYi7qYIw5oltiZpw_y5zWoMC0e4pBEpPuBE7oi-P5_rgrrKKatPk_AZ7UNweODxpnx8gotj04O0w/s0/Mumbai_Indians_flag.gif",
+        "title": "5 titles (2013 , 2015 , 2017 , 2019 , 2020)",
+
+    },
+    {
+        "team": "KKR",
+        "primary-color": "purple",
+        "secondary-color": "gold",
+        "captain": "Ajinkya Rahane",
+        "image": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEil9bkQO4L4cWDQpBjVmv9MEk6_GG1xya08SPeGnaDNxzQkWaqHylQ4A42o59WG-BOpZmjC3-zhjzHWeTJ9eJa1OABJ_gMZsovQEVcXXZqzoY-SeXDRmUAHklh7jJKJHh20IhJlARKp3y_V9vUVr7halYAjtJhVOyjL_fzblVwwKxUoibgQQnyx_fKF1Fw/s600/Kolkata_Knight_Riders_flag_vertical.gif",
+        "title": "3 titles (2012 , 2014 , 2024)",
+
+    },
+    {
+        "team": "DC",
+        "primary-color": "blue",
+        "secondary-color": "red",
+        "captain": "Axar Patel",
+        "image": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgXT9SrB0Xib06FQspyl7eTmsnrGHC-HBKfN038iyQd10mpmlmFPhSG_QJ-uNerV3vSIr9mg34HEWsdzhLE0vSz5JrL9QLj5RAUnT2lqVjR3W4E9JxlJewraAncw5cZ3p7sqRkRr9tCoaI/s0/Delhi_Capitals_flag.gif",
+        "title": "0 titles",
+
+
+    },
+    {
+        "team": "RR",
+        "primary-color": "pink",
+        "secondary-color": "blue",
+        "captain": "Sanju Samson",
+        "image": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjRJb59Hgk5UHEWEwRUsE_nHHPI3rI__qUIDV3QJ-anM0SwYOy8_NJdxsTH7bS0QM7HlP81mmipePF3eRVk_r38tm4x6Hkei6Lr9LHkTNaF5o4AAB2agpU1dPY3sagPRBT6E2dwdCB5ViY/s0/Lucknow_Super_Giants_flag.gif",
+        "title": "1 title (2008)",
+
+
+    },
+    {
+        "team": "GT",
+        "primary-color": "royal blue",
+        "secondary-color": "black",
+        "captain": "Shubman Gill",
+        "image": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiHR89DKxYH9EvTN7yJ8U9Prtxp94Iof-HQRKPXo690oJ6HOhw4GjRdommzUTNjk51Nl3QZDuQKfPhrUlVmRzvWvOZI3Yh-SA5sM7coR0MHYXxPz9wiwHoDuyhv6cnYrfYDclmvFxGhqb0/s0/Gujarat_Titans_flag.gif",
+        "title": "1 title (2022)",
+
+    },
+    {
+        "team": "RCB",
+        "primary-color": "red",
+        "secondary-color": "black",
+        "captain": "Rajat Patidar",
+        "image": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhQa6spVZUUSINP7LqOh7CGq0DacY0xy85NH7dOCfkNyNY2fGZFQqzIxPaxj3nBY3siSsEzg07-qgEnv0DNpDo3yVbX4wenCNY1h0Hu0uj7Cv00DzNyyxzVy-ed2U__GEoCIzhLodoUmbw/s0/Royal_Challengers_Bangalore_flag.gif",
+        "title": "1 title (2025)",
+
+    },
+    {
+        "team": "Lsg",
+        "primary-color": "lightblue",
+        "secondary-color": "red",
+        "captain": "Rishabh Pant",
+        "image": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjRJb59Hgk5UHEWEwRUsE_nHHPI3rI__qUIDV3QJ-anM0SwYOy8_NJdxsTH7bS0QM7HlP81mmipePF3eRVk_r38tm4x6Hkei6Lr9LHkTNaF5o4AAB2agpU1dPY3sagPRBT6E2dwdCB5ViY/s0/Lucknow_Super_Giants_flag.gif",
+        "title": "0 titles ",
+
+    },
+    {
+        "team": "SRH",
+        "primary-color": "orange",
+        "secondary-color": "red",
+        "captain": "Pat Cummins",
+        "image": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjEgurTFtjLo9YusjeGPb4mSMCuRybNM7W1N4luW2g16SzSDPl8YdkPD8_Tuha2JqSukzNRuc4csSwiZak9Ixn0eRGBfKVtDl5onfRmMXLoxo6iEDcvZmxO3r2IxSSLymJuIqHL5ol-DBQ/s0/Sunrisers_Hyderabad_flag.gif",
+        "title": "1 title (2016)",
+
+    },
+    {
+        "team": "PBKS",
+        "primary-color": "gold",
+        "secondary-color": "red",
+        "captain": "Rishabh Pant",
+        "image": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjRJb59Hgk5UHEWEwRUsE_nHHPI3rI__qUIDV3QJ-anM0SwYOy8_NJdxsTH7bS0QM7HlP81mmipePF3eRVk_r38tm4x6Hkei6Lr9LHkTNaF5o4AAB2agpU1dPY3sagPRBT6E2dwdCB5ViY/s0/Lucknow_Super_Giants_flag.gif",
+        "title": "0 titles",
+
+    },
+
+
+];
+let main = document.querySelector("main");
+let box = document.querySelector(".box");
+let h1 = document.querySelector("h1");
+let h3 = document.querySelector("h3");
+let h4 = document.querySelector("h4");
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+
+    let number = Math.floor(Math.random() * arr.length)
+
+    h1.innerHTML = arr[number].team
+    h3.innerHTML = arr[number].captain
+    h4.innerHTML = arr[number].title
+    box.style.backgroundColor = arr[number]["primary-color"]
+    main.style.backgroundImage = `url('${arr[number].image}')`;
+
+
+
+    main.style.backgroundSize = "cover";
+    main.style.backgroundPosition = "center";
+    // main.style.backgroundRepeat = "no-repeat";
+
+
+
+
+
+})
